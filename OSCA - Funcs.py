b150ae83b094b6946ecc5f3722b58ee6df2c223a
utahjohnzz <126109512+utahjohnzz@users.noreply.github.com>
2024-02-26 19:20:08 -0500

    Update OSCA - Funcs.py

--- OSCA - Funcs.py
+++ OSCA - Funcs.py
@@ -8,8 +8,10 @@ Created on Sun Feb 11 19:29:11 2024
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.stats as stats
+import math
 from scipy.stats import norm
 from matplotlib.ticker import MultipleLocator
+from scipy.stats import shapiro
 #####################################
 #Generated Data
 #####################################
@@ -42,29 +44,40 @@ data = np.array([
     [10.05, 10.05, 10.01, 10.00, 10.04]
 ])
 
+# Define the desired range
+lower_bound = 4.9
+upper_bound = 5.34
+
+# Calculate the mean and standard deviation of the range
+mean_range = (upper_bound + lower_bound) / 2
+std_range = (upper_bound - lower_bound) / 6  # Using 6 standard deviations to cover most of the range
+
+# Generate data
+#data = np.random.normal(loc=mean_range, scale=std_range, size=(20, 5))
+data = np.random.uniform(low=lower_bound, high=upper_bound, size=(20, 5))
 #norm prob data
-#upper limit
-usl=10.05
+#upper control limit
 #lower limit
-lsl=9.95
-#calc mean for example data based on usl,lsl
-mean = (usl + lsl) / 2  
-std_dev = (usl - lsl) / 6
+#calc mean for example data based on ucl,lcl
+mean = np.mean(data)
+sigma= np.std(data)
+ucl=np.round(mean+3*sigma,3)
+lcl=np.round(mean-3*sigma,3)
 
 
 #function for calling
-def hist(data,usl,lsl):
+def hist(data,ucl,lcl):
     #############Histogram
     #plots histogram
     data=data.flatten()
     #plt.hist(data, bins=10, density=True, alpha=0.6, color='blue', edgecolor='black')
     #calculates mean and std dev of data input
     mu, sigma = np.mean(data), np.std(data)
-    #plots the USL and LSL lines on the plot
-    #plt.axvline(x=usl, color='red', linestyle='--', linewidth=2)
-    #plt.text(usl,plt.gca().get_ylim()[1], f'USL= {usl}', fontsize=12, color='r')
-    #plt.axvline(x=lsl, color='red', linestyle='--', linewidth=2)
-    #plt.text(lsl,plt.gca().get_ylim()[1], f'LSL= {lsl}', fontsize=12, color='r')
+    #plots the ucl and lcl lines on the plot
+    #plt.axvline(x=ucl, color='red', linestyle='--', linewidth=2)
+    #plt.text(ucl,plt.gca().get_ylim()[1], f'ucl= {ucl}', fontsize=12, color='r')
+    #plt.axvline(x=lcl, color='red', linestyle='--', linewidth=2)
+    #plt.text(lcl,plt.gca().get_ylim()[1], f'lcl= {lcl}', fontsize=12, color='r')
 
     xmin, xmax = np.min(data),np.max(data)
     xhist = np.linspace(xmin, xmax, 100)
@@ -92,16 +105,16 @@ def xbar(data):
     #c=1
     #to further distinguish subgroups that are out of control, we will indicate these as a red dot
     #for i in means:
-        #if i>=usl or i<=lsl:
+        #if i>=ucl or i<=lcl:
             #plt.plot(c,i,marker='o',color='r')
         #c+=1
         
     #the next following lines are for adding text onto the plot to distinguish the line meanings.
-    #plt.ylim(lsl-std_dev,usl+std_dev)
-    #plt.axhline(y=usl,color='r')
-    #plt.axhline(y=lsl,color='r')
-    #plt.text(sn+1.2,usl-.002, f'UCL= {usl}',color='r', fontsize=12)
-    #plt.text(sn+1.2,lsl-.002, f'LCL= {lsl}',color='r', fontsize=12)
+    #plt.ylim(lcl-sigma,ucl+sigma)
+    #plt.axhline(y=ucl,color='r')
+    #plt.axhline(y=lcl,color='r')
+    #plt.text(sn+1.2,ucl-.002, f'UCL= {ucl}',color='r', fontsize=12)
+    #plt.text(sn+1.2,lcl-.002, f'LCL= {lcl}',color='r', fontsize=12)
     #plt.axhline(y=xbarr,color='g')
     #plt.text(sn+1.2,xbarr-.002, r'$\overline{\overline{x}}$' + f' = {np.round(xbarr,3)}', fontsize=12)
     #plt.ylabel('Sample Mean',fontsize=12)
@@ -116,7 +129,7 @@ def rbar(data):
     sx=np.linspace(1,sn,sn)
     sx=sx.astype(int)
     ranges = [np.ptp(row) for row in data]
-    std_devm=np.std(ranges)
+    sigmam=np.std(ranges)
     #we also need to calculate the correct control limits.
     #to do so, we need to incorporate the usage of control constants based upon subgroup size.
     #we already know subgroup size.
@@ -166,10 +179,10 @@ def rbar(data):
         d4=1.541
     #now we can calculate the ucl and lcl for the R chart
     rbarr=np.sum(ranges)/np.size(ranges)
-    uclr=d4*rbarr
-    lclr=d3*rbarr
+    uclr=np.round(d4*rbarr,3)
+    lclr=np.round(d3*rbarr,3)
     #plt.plot(sx,ranges,marker='o')
-    #plt.ylim(-.01,np.max(ranges)+std_devm)
+    #plt.ylim(-.01,np.max(ranges)+sigmam)
     #plt.xticks(sx,sx)
     #plt.axhline(uclr,color='r')
     #plt.axhline(lclr,color='r')
@@ -182,7 +195,7 @@ def rbar(data):
         #c+=1
     #plt.title('R Chart',fontsize=14)
     #plt.ylabel('Sample Range',fontsize=12)
-    return ranges, uclr, lclr, std_devm, rbarr
+    return ranges, uclr, lclr, sigmam, rbarr
 
 #Subgroup Plotting
 #we just need to plot a scatter for this one.
@@ -198,8 +211,8 @@ def last(data):
         #if c==sn:
             #break
     #plt.xticks(sx)
-    #plt.yticks([np.round(np.mean(data)-std_dev*3,2),np.round(np.mean(data)+std_dev*3,2)])
-    #plt.ylim(np.min(data-std_dev*3),np.max(data+std_dev*3))
+    #plt.yticks([np.round(np.mean(data)-sigma*3,2),np.round(np.mean(data)+sigma*3,2)])
+    #plt.ylim(np.min(data-sigma*3),np.max(data+sigma*3))
     #plt.axhline(np.mean(data),linestyle='--',alpha=.5)
     #plt.gca().set_aspect(40)
     #plt.title(f'Last {sn}' +' Subgroups',fontsize=14)
@@ -221,7 +234,7 @@ def prob(data):
     result = stats.anderson(flatdata, dist='norm')
     ad = result.statistic
 
-    #plt.xlim(np.min(sdata)-std_dev/2,np.max(sdata+std_dev/2))
+    #plt.xlim(np.min(sdata)-sigma/2,np.max(sdata+sigma/2))
     #plt.ylim(0,np.max(pr))
     lr=np.polyfit(sdata,pr,1)
     lrdata=lr[0]*sdata+lr[1]
@@ -236,62 +249,65 @@ def prob(data):
     #plt.yticks([])
     #plt.title('Normal Probability Plot')
     #plt.grid(True)
-    statistic, p = stats.shapiro(data)
 
-    #plt.text(np.max(sdata)+std_dev*5/4, .5, f'Mean: {np.round(np.mean(data),3)} \nStandard Deviation: {np.round(np.std(data),3)} \nN: {len(data)}\nAD:{np.round(ad,3)}\nP-Value:{np.round(p,3)}', fontsize=10, bbox=dict(facecolor='white', alpha=0.5))
+    statistic, p = shapiro(data)
+
+
+    #plt.text(np.max(sdata)+sigma*5/4, .5, f'Mean: {np.round(np.mean(data),3)} \nStandard Deviation: {np.round(np.std(data),3)} \nN: {len(data)}\nad:{np.round(ad,3)}\nP-Value:{np.round(p,3)}', fontsize=10, bbox=dict(facecolor='white', alpha=0.5))
     return sdata,pr,fdata,lrdata,p,flatdata,ad
 
-def osca(data,usl,lsl):
-    xhist, phist, sigma, mu=hist(data,usl,lsl)
+def osca(data,ucl,lcl):
+    xhist, phist, sigma, mu=hist(data,ucl,lcl)
     sn,ss,sx,mean,stdw,means,xbarr=xbar(data)
-    ranges, uclr, lclr, std_devm, rbarr =rbar(data)
+    ranges, uclr, lclr, sigmam, rbarr =rbar(data)
     last(data)
     sdata,pr,fdata,lrdata,p,flatdata,ad=prob(data)
     wsigma=np.mean(stdw)
-    cp=(usl-lsl)/(6*wsigma)
-    cpk=np.min([(usl-mu),(mu-lsl)])/(3*wsigma)
+    cp=(ucl-lcl)/(6*wsigma)
+    cpk=np.min([(ucl-mu),(mu-lcl)])/(3*wsigma)
     dp=0
     for i in flatdata:
-        if i < lsl or i > usl:
+        if i < lcl or i > ucl:
             dp+=1
     dpu=dp/len(flatdata)
     ppm=dp/len(flatdata)*1000000
-    pp=(usl-lsl)/(6*std_dev)
-    if np.abs(mu-usl)>np.abs(mu-lsl):
-        ppk=(mu-lsl)/(3*std_dev)
+    pp=(ucl-lcl)/(6*sigma)
+    if np.abs(mu-ucl)>np.abs(mu-lcl):
+        ppk=(mu-lcl)/(3*sigma)
     else:
-        ppk=(usl-mu)/(3*std_dev)
+        ppk=(ucl-mu)/(3*sigma)
     fig, axs = plt.subplots(3, 2, figsize=(10, 10))
     axs[0,0].plot(xhist, phist, 'k', linewidth=2)
     axs[0,0].set_xlabel('Value')
     axs[0,0].set_ylabel('Frequency')
-    axs[0,0].axvline(x=usl,color='r')
-    axs[0,0].axvline(x=lsl,color='r')
+    axs[0,0].axvline(x=ucl,color='r')
+    axs[0,0].axvline(x=lcl,color='r')
     axs[0,0].hist(flatdata,bins=12,edgecolor='black',color='blue')
     axs[0,0].set_title('Capability Histogram',fontsize=14)
-    axs[0, 0].text(lsl+std_dev/6, np.max(flatdata)*2.5, 'LCL', fontsize=12, color='red')
-    axs[0, 0].text(usl+std_dev/6, np.max(flatdata)*2.5, 'UCL', fontsize=12, color='red')
+    axs[0, 0].text(lcl+sigma/6, np.max(flatdata)*2.5, 'LCL', fontsize=12, color='red')
+    axs[0, 0].text(ucl+sigma/6, np.max(flatdata)*2.5, 'UCL', fontsize=12, color='red')
     plt.subplots_adjust(hspace=0.5, wspace=0.5)
     axs[2, 1].axis('off')
     
     axs[0,1].plot(sx,means,marker='o')
+    axs[0,1].set_ylabel('Sample Mean',fontsize=12)
     axs[0,1].axhline(y=xbarr,color='g')
-    axs[0,1].set_ylim(lsl-std_dev,usl+std_dev)
-    axs[0,1].axhline(y=usl,color='r')
-    axs[0,1].axhline(y=lsl,color='r')
-    axs[0,1].text(sn+1.2,usl-.002, f'UCL= {usl}',color='r', fontsize=12)
+    axs[0,1].set_ylim(lcl-sigma,ucl+sigma)
+    axs[0,1].axhline(y=ucl,color='r')
+    axs[0,1].axhline(y=lcl,color='r')
+    axs[0,1].text(sn+1.2,ucl-.002, f'UCL= {ucl}',color='r', fontsize=12)
     axs[0,1].text(sn+1.2,xbarr-.002, r'$\overline{\overline{x}}$' + f' = {np.round(xbarr,3)}', fontsize=12)
-    axs[0,1].text(sn+1.2,lsl-.002, f'LCL= {lsl}',color='r', fontsize=12)
+    axs[0,1].text(sn+1.2,lcl-.002, f'LCL= {lcl}',color='r', fontsize=12)
     axs[0,1].set_title('Xbar Chart',fontsize=14)
     c=1
     for i in means:
-        if i>=usl or i<=lsl:
+        if i>=ucl or i<=lcl:
             axs[0,1].plot(c,i,marker='o',color='r')
         c+=1
     
     
     axs[1,1].plot(sx,ranges,marker='o')
-    axs[1,1].set_ylim(-.01,np.max(ranges)+std_devm)
+    axs[1,1].set_ylim(-.01,np.max(ranges)+sigmam)
     axs[1,1].set_xticks(sx.astype(int),sx.astype(int))
     xmin = min(sx.astype(int))
     xmax = max(sx.astype(int))
@@ -300,10 +316,10 @@ def osca(data,usl,lsl):
     axs[1, 1].set_xticklabels(tick_labels)
     
 
-    axs[1, 1].tick_params(axis='x', pad=10)  # Adjust the value of 'pad' as needed
+    axs[1, 1].tick_params(axis='x', pad=10)  # adjust the value of 'pad' as needed
     
-    axs[1,1].text(sn+1.2,uclr-.002, f'UCL= {usl}',color='r', fontsize=12)
-    axs[1,1].text(sn+1.2,lclr-.002, f'LCL= {lsl}',color='r', fontsize=12)
+    axs[1,1].text(sn+1.2,uclr-.002, f'UCL= {uclr}',color='r', fontsize=12)
+    axs[1,1].text(sn+1.2,lclr-.002, f'LCL= {lclr}',color='r', fontsize=12)
     axs[1,1].axhline(y=uclr,color='r')
     axs[1,1].axhline(y=lclr,color='r')
     axs[1,1].axhline(y=rbarr,color='g')
@@ -324,10 +340,10 @@ def osca(data,usl,lsl):
         if c==sn:
             break
     axs[1,0].set_xticks(sx)
-    axs[1,0].set_yticks([np.round(np.mean(data)-std_dev*3,2),np.round(np.mean(data)+std_dev*3,2)])
-    axs[1,0].set_ylim(np.min(data-std_dev*3),np.max(data+std_dev*3))
+    axs[1,0].set_yticks([np.round(np.mean(data)-sigma*3,2),np.round(np.mean(data)+sigma*3,2)])
+    axs[1,0].set_ylim(np.min(data-sigma*3),np.max(data+sigma*3))
     axs[1,0].axhline(np.mean(data),linestyle='--',alpha=.5)
-    axs[1,0].set_aspect(40)
+    #axs[1,0].set_aspect(40)
     axs[1,0].set_title(f'Last {sn}' +' Subgroups',fontsize=14)
     axs[1,0].set_ylabel('Values',fontsize=12)
     axs[1,0].set_xlabel('Sample',fontsize=12)
@@ -335,7 +351,7 @@ def osca(data,usl,lsl):
     
     
     
-    axs[2,0].set_xlim(np.min(sdata)-std_dev/2,np.max(sdata+std_dev/2))
+    axs[2,0].set_xlim(np.min(sdata)-sigma/2,np.max(sdata+sigma/2))
     axs[2,0].set_ylim(0,np.max(pr)+np.max(pr)/2)
     lr=np.polyfit(sdata,pr,1)
     #axs[2,0].plot(sdata,fdata,color='g',alpha=.4,linewidth=3)
@@ -347,21 +363,21 @@ def osca(data,usl,lsl):
     axs[2,0].set_title('Normal Probability Plot')
     axs[2,0].grid(True)
 
-    axs[2,0].text(np.max(sdata)+std_dev*5/4, .35, f'Mean: {np.round(np.mean(data),3)} \nStandard Dev: {np.round(np.std(data),3)} \nN: {np.size(data)}\nAD:{np.round(ad,3)}\nP-Value:{np.round(p,3)}', fontsize=10, bbox=dict(facecolor='white', alpha=0.5))
+    axs[2,0].text(np.max(sdata)+sigma*5/4, .35, f'Mean: {np.round(np.mean(data),3)} \nStandard Dev: {np.round(np.std(data),3)} \nN: {np.size(data)}\nAD:{np.round(ad,3)}\nP-Value:{np.round(p,3)}', fontsize=10, bbox=dict(facecolor='white', alpha=0.5))
     
     
 
     
     
     
-    axs[0, 0].text(-.25, 1.4, 'Open Source Capability Analysis (OSCA) Report', transform=axs[0, 0].transAxes, ha='left', va='center', fontsize=20,fontstyle='italic')
+    axs[0, 0].text(-.25, 1.4, 'Open Source Capability Analysis (OSCA) Report',transform=axs[0, 0].transAxes, ha='left', va='center', fontsize=20,fontstyle='italic')
 
 
     
     
     
     
-    text_box = axs[2, 1].text(2, .6, 'Process Metrics \n' + f'Standard Deviation= {np.round(std_dev,3)}\n' + f'Cp= {np.round(cp,3)}\n' + f'Cpk= {np.round(cpk,3)}\n'+ f'Pp= {np.round(pp,3)}\n'+ f'Ppk= {np.round(ppk,3)}\n'+ f'Ppm= {np.round(ppm,3)}',
+    text_box = axs[2, 1].text(2, .6, 'Process Metrics \n' + f'Standard Deviation= {np.round(sigma,3)}\n' + f'Cp= {np.round(cp,3)}\n' + f'Cpk= {np.round(cpk,3)}\n'+ f'Pp= {np.round(pp,3)}\n'+ f'Ppk= {np.round(ppk,3)}\n'+ f'Ppm= {np.round(ppm,3)}',
                            horizontalalignment='center',
                            verticalalignment='center',
                            transform=axs[2, 0].transAxes,
@@ -375,7 +391,7 @@ def osca(data,usl,lsl):
     
     
 
-osca(data,usl,lsl)
+osca(data,ucl,lcl)
 
 
 
